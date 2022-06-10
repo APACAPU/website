c1bdb777f260975e9c27635d800ede209bb6219b
HohShenYien <hohshenyien@gmail.com>
2022-06-10 22:29:48 +0800

    Simple fix for og meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout";
 import theme from "../styles/CustomColor";
 import {useEffect} from "react";
 import Router, {useRouter} from "next/router";
+import Head from "next/head";
 
 const routeChange = () => {
     // Temporary fix to avoid flash of unstyled content
@@ -31,12 +32,25 @@ export default function MyApp({ Component, pageProps }) {
         router.push(router.pathname);
         document.body.style.setProperty("margin", '0', 'important');
     }, []);
-    
+
   return (
-      <ChakraProvider theme={theme}>
-          <Layout>
-              <Component {...pageProps} />
-          </Layout>
-      </ChakraProvider>
+      <>
+          <Head>
+              <title>Home Page | APAC</title>
+              <meta name="description" content="APAC - We are an analytics club aiming to bring more exposure to APU students about Data Science."/>
+              <link rel="icon" href="/favicon.ico"/>
+              <meta property="og:title" content="Home Page | APAC" />
+              <meta property="og:description" content="APAC - We are an analytics club aiming to bring more exposure to APU students about Data Science." />
+              <meta property="og:image" content="https://i.imgur.com/qsEF6vV.png"/>
+              <meta property="twitter:title" content="Home Page | APAC"/>
+              <meta property="twitter:description" content="APAC - We are an analytics club aiming to bring more exposure to APU students about Data Science." />
+              <meta property="twitter:card" content="summary_large_image"/>
+          </Head>
+          <ChakraProvider theme={theme}>
+              <Layout>
+                  <Component {...pageProps} />
+              </Layout>
+          </ChakraProvider>
+      </>
   )
 }
